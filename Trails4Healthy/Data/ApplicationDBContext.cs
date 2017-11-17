143b92cade2732a01a92470bd2a18ae104bbbe3a
miguelalmeida10 <33085795+miguelalmeida10@users.noreply.github.com>
2017-11-17 12:55:07 +0000

    Criação de Models trail, difficulty e status, Fundação para a Base de Dados e pequenas melhorias

---
+++ Trails4Healthy/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Trails4Healthy.Models;
+
+namespace Trails4Healthy.Data
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<CreateTrail> Trails { get; set; }
+    }
+}
