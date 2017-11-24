a5d3a1a33dba14ff2ca20d70c4cf1daeb6ca6694
paloma316 <33006522+paloma316@users.noreply.github.com>
2017-11-24 13:22:32 +0000

    Criacao da Classe Base de Dados

---
+++ Trails4Healthy/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Healthy.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(
+DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+    }
+}
