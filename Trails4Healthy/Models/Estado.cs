67b506ac693667def079eccd79be4dd4a74b4edb
paloma316 <33006522+paloma316@users.noreply.github.com>
2017-11-29 10:12:37 +0000

    Criacao de Tabelas

--- Trails4Healthy/Models/Estado.cs
+++ Trails4Healthy/Models/Estado.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Trails4Healthy.Models
     public class Estado
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EstadoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
