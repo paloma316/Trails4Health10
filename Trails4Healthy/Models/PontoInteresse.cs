ec100a23261925cccd6ec1f7af7216456fab73dd
VeraSantos2017 <ninitasantos.15@hotmail.com>
2017-11-29 10:22:51 +0000

    Alteraçao base de dados
    alteração do campo localidade para coordena na tabela ponto de interessse e eliminação do campo estação da tabela categorias

---
+++ Trails4Healthy/Models/PontoInteresse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Healthy.Models
+{
+    public class PontoInteresse
+    {
+    }
+}
