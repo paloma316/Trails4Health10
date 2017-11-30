90f07b9467ff8f58174253840081560ae60f7ff2
VeraSantos2017 <32763946+VeraSantos2017@users.noreply.github.com>
2017-11-30 12:57:16 +0000

    PI classe trilhoPonto
    criação da classe que liga a classe trilho com ponto de interesse
    
    campos
    IDPontoInteresse
    TrailID

--- Trails4Healthy/Models/Local.cs
+++ Trails4Healthy/Models/Local.cs
@@ -9,7 +9,7 @@ namespace Trails4Healthy.Models
     {
         public int IDLocalização { get; set; }// chave primaria
         public string nomeLocalidade { get; set; }//
-        public string conselho{ get; set; }//
+        public string concelho{ get; set; }//
         public string distrito{ get; set; }//
         public int coordenadas { get; set; }// coordenada de gps do local
         public string descricao { get; set; }// pequeno texto com informaçoes sobre o local
---
+++ Trails4Healthy/Models/TrilhosPontos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Healthy.Models
+{
+    public class TrilhosPontos
+    {
+        public int IDPontoInteresse { get; set; } // chave concatenada ponto de interesse
+        public int TrailID { get; set; }// chave concatenada trilho
+    }
+}
