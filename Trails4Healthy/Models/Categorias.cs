8e70432168c2614f5a17a7ce237aea2b223b108b
paloma316 <33006522+paloma316@users.noreply.github.com>
2017-12-05 23:12:45 +0000

    Revert "Revert "Revert "Merge branch 'BasedeDados' of https://github.com/paloma316/Trails4Health10 into BasedeDados"""
    This reverts commit c03c73b673b2f2f714aa939b6cb75ed2ecc5a4b7.

---
+++ Trails4Healthy/Models/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Healthy.Models
+{
+    public class Categorias
+    {
+        public int IDCategoria { get; set; }// chave primaria 
+        public string nomeCategoria { get; set; }// 
+        public int IDCategoria { get; set; }//
+    }
+}
